a3b39413dd74a3a3a80d7b45a4d6e7ede757285f
DaoAnhTu2105 <tudase151149@gmail.com>
2023-09-08 15:56:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { RouterComponents } from "./routers";
+
+function App() {
+  return (
+    <div>
+      <RouterComponents/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routers/index.js
@@ -0,0 +1,131 @@
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import DefaultLayout from "../components/DefaultLayout";
+import Home from "../pages/Home";
+
+export const publicRouters = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+];
+
+export const privateRouters = [];
+
+export const adminRouters = [];
+
+export const cookerRouters = [];
+
+export const userRouters = [];
+
+//Scroll Top when clicked another page
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
+
+export const RouterComponents = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <ScrollToTop />
+        <Routes>
+          {publicRouters.map((route, index) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+          {/* <Route exact path="/" element={<PrivateRouters />}>
+                        {privateRouters.map((route, index) => {
+                            const Page = route.component
+                            let Layout = DefaultLayout
+                            if (route.layout) {
+                                Layout = route.layout
+                            } else if (route.layout === null) {
+                                Layout = Fragment
+                            }
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    }
+                                />
+                            )
+                        })}
+                    </Route>
+                    <Route exact path="/" element={<AdminRouters />}>
+                        {adminRouters.map((route, index) => {
+                            const Page = route.component
+                            let Layout = DefaultLayout
+                            if (route.layout) {
+                                Layout = route.layout
+                            } else if (route.layout === null) {
+                                Layout = Fragment
+                            }
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    }
+                                />
+                            )
+                        })}
+                    </Route>
+                    <Route exact path="/" element={<UserRouters />}>
+                        {userRouters.map((route, index) => {
+                            const Page = route.component
+                            let Layout = DefaultLayout
+                            if (route.layout) {
+                                Layout = route.layout
+                            } else if (route.layout === null) {
+                                Layout = Fragment
+                            }
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    }
+                                />
+                            )
+                        })}
+                    </Route> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
