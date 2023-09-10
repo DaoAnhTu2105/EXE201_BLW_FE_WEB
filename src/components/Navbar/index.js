91cdf9c476ccf52505e2f8a8442f4f4c9c4f4006
DaoAnhTu2105 <tudase151149@gmail.com>
2023-09-10 12:37:46 +0700

    add homepage layout

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,38 +3,44 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-            />
-          </a>
+      <div style={{ position: "fixed", zIndex: 1000, width: "100%" }}>
+        <nav
+          className="navbar"
+          role="navigation"
+          aria-label="main navigation"
+          style={{ paddingTop: 20, paddingBottom: 10 }}
+        >
+          <div className="navbar-brand">
+            <a className="navbar-item" href="https://bulma.io">
+              <img
+                src="https://bulma.io/images/bulma-logo.png"
+                width="112"
+                height="28"
+              />
+            </a>
 
-          <a
-            role="button"
-            className="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
+            <a
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
 
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">Thức ăn</a>
-            <a className="navbar-item">Thực đơn</a>
-            <a className="navbar-item">Kế hoạch</a>
-            <a className="navbar-item">Chuyên gia</a>
-            <a className="navbar-item">Cộng đồng</a>
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item">Thức ăn</a>
+              <a className="navbar-item">Thực đơn</a>
+              <a className="navbar-item">Kế hoạch</a>
+              <a className="navbar-item">Chuyên gia</a>
+              <a className="navbar-item">Cộng đồng</a>
 
-            {/* <div className="navbar-item has-dropdown is-hoverable">
+              {/* <div className="navbar-item has-dropdown is-hoverable">
               <a className="navbar-link">More</a>
               <div className="navbar-dropdown">
                 <a className="navbar-item">About</a>
@@ -44,20 +50,21 @@ const Navbar = () => {
                 <a className="navbar-item">Report an issue</a>
               </div>
             </div> */}
-          </div>
+            </div>
 
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <a className="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a className="button is-light">Log in</a>
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <a className="button is-primary">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a className="button is-light">Log in</a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 };
