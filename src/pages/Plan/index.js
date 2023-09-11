2eab2a0f3115142cf0c0e05772bef9ca849e4f31
DaoAnhTu2105 <tudase151149@gmail.com>
2023-09-11 21:14:30 +0700

    add layout plan test the same in figma

---
+++ src/pages/Plan/index.js
@@ -0,0 +1,101 @@
+import React from "react";
+
+const Plan = () => {
+  return (
+    <div class="container mb-5">
+      <div class="container is-max-desktop">
+        <div class="notification is-primary is-fullheight">
+          <h3 class="title is-3 has-text-dark">
+            Kế hoạch hướng dẫn ăn uống chi tiết cho con của bạn
+          </h3>
+          <div class="box">
+            <a href="/" style={{ textDecoration: "none" }}>
+              <h5
+                class="title is-5 has-text-dark"
+                style={{ textAlign: "center" }}
+              >
+                6 tháng - 30 ngày đầu tiên
+              </h5>
+            </a>
+          </div>
+          <div class="columns">
+            <div class="column is-half">
+              <div class="box mb-0">
+                <a href="/" style={{ textDecoration: "none" }}>
+                  <h5
+                    class="title is-5 has-text-dark"
+                    style={{ textAlign: "center" }}
+                  >
+                    7 tháng tuổi
+                  </h5>
+                </a>
+              </div>
+            </div>
+            <div class="column is-half">
+              <div class="box mb-0">
+                <a href="/" style={{ textDecoration: "none" }}>
+                  <h5
+                    class="title is-5 has-text-dark"
+                    style={{ textAlign: "center" }}
+                  >
+                    8 tháng tuổi
+                  </h5>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column is-one-third">
+              <div class="box mb-0">
+                <a href="/" style={{ textDecoration: "none" }}>
+                  <h5
+                    class="title is-5 has-text-dark"
+                    style={{ textAlign: "center" }}
+                  >
+                    9 tháng tuổi
+                  </h5>
+                </a>
+              </div>
+            </div>
+            <div class="column is-one-third">
+              <div class="box mb-0">
+                <a href="/" style={{ textDecoration: "none" }}>
+                  <h5
+                    class="title is-5 has-text-dark"
+                    style={{ textAlign: "center" }}
+                  >
+                    10 tháng tuổi
+                  </h5>
+                </a>
+              </div>
+            </div>
+            <div class="column is-one-third">
+              <div class="box mb-0">
+                <a href="/" style={{ textDecoration: "none" }}>
+                  <h5
+                    class="title is-5 has-text-dark"
+                    style={{ textAlign: "center" }}
+                  >
+                    11 tháng tuổi
+                  </h5>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="box">
+            <a href="/" style={{ textDecoration: "none" }}>
+              <h5
+                class="title is-5 has-text-dark"
+                style={{ textAlign: "center" }}
+              >
+                12 tháng trở lên và bắt đầu ăn cùng gia đình
+              </h5>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Plan;
